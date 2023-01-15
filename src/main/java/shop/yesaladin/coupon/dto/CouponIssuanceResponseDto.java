4d9b99c4ecbd1471ad104d753ceb285bf4ec2646
Hongdae Kim <mongmeo.dev@gmail.com>
2023-01-16 00:29:27 +0900

    [Docs] : javadoc 추가

--- src/main/java/shop/yesaladin/coupon/dto/CouponIssuanceResponseDto.java
+++ src/main/java/shop/yesaladin/coupon/dto/CouponIssuanceResponseDto.java
@@ -4,6 +4,12 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * 쿠폰 발행 후 발행된 쿠폰 코드들을 저장하는 DTO 클래스입니다.
+ *
+ * @author 김홍대
+ * @since 1.0
+ */
 @Getter
 @AllArgsConstructor
 public class CouponIssuanceResponseDto {
