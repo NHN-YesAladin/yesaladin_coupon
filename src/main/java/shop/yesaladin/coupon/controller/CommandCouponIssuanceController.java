86d88717f38a37bd398c853f2e5ad0f8aece0db7
Hongdae Kim <mongmeo.dev@gmail.com>
2023-01-17 09:51:15 +0900

    [Docs] : CommandCouponIssuanceController javadoc 추가

--- src/main/java/shop/yesaladin/coupon/controller/CommandCouponIssuanceController.java
+++ src/main/java/shop/yesaladin/coupon/controller/CommandCouponIssuanceController.java
@@ -12,6 +12,12 @@ import shop.yesaladin.coupon.dto.CouponIssuanceRequestDto;
 import shop.yesaladin.coupon.dto.CouponIssuanceResponseDto;
 import shop.yesaladin.coupon.service.inter.CommandCouponIssuanceService;
 
+/**
+ * 쿠폰 발급 관련 Command 요청을 처리하는 Controller 클래스입니다.
+ *
+ * @author 김홍대
+ * @since 1.0
+ */
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/v1/coupons/issuances")
@@ -19,6 +25,12 @@ public class CommandCouponIssuanceController {
 
     private final CommandCouponIssuanceService issuanceCommandService;
 
+    /**
+     * 쿠폰을 발행합니다.
+     *
+     * @param dto 쿠폰 발행 정보를 담은 dto
+     * @return 발행된 쿠폰 번호가 담긴 DTO
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CouponIssuanceResponseDto issueCoupon(@Valid @RequestBody CouponIssuanceRequestDto dto) {
