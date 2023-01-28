30e94cc8ef19fc56d5db934b738c6d992eb40d5b
Hongdae Kim <mongmeo.dev@gmail.com>
2023-01-28 17:23:05 +0900

    [Docs] : Javadoc 추가

--- src/main/java/shop/yesaladin/coupon/validator/annotation/EnumValue.java
+++ src/main/java/shop/yesaladin/coupon/validator/annotation/EnumValue.java
@@ -9,6 +9,12 @@ import javax.validation.Constraint;
 import javax.validation.Payload;
 import shop.yesaladin.coupon.validator.validator.EnumValidator;
 
+/**
+ * enum 타입의 Bean Validation 을 위한 어노테이션입니다. String 으로 들어온 값이 enum 클래스에 존재하는지 확인합니다.
+ *
+ * @author 김홍대
+ * @since 1.0
+ */
 @Documented
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE,
         ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
--- src/main/java/shop/yesaladin/coupon/validator/validator/EnumValidator.java
+++ src/main/java/shop/yesaladin/coupon/validator/validator/EnumValidator.java
@@ -6,6 +6,13 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import shop.yesaladin.coupon.validator.annotation.EnumValue;
 
+/**
+ * enum 타입의 Bean Validation 을 위한 Validator 입니다. String 으로 들어온 값이 enum 클래스에 존재하는지 확인합니다. null 값이 들어온
+ * 경우 유효성 검증을 실행하지 않고 유효한 값으로 판단합니다.
+ *
+ * @author 김홍대
+ * @since 1.0
+ */
 public class EnumValidator implements ConstraintValidator<EnumValue, String> {
 
     private EnumValue annotation;
