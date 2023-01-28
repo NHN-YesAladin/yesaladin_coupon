3b012f4c02c9809cd90f99188a54cc2b734f564c
Hongdae Kim <mongmeo.dev@gmail.com>
2023-01-28 16:08:39 +0900

    [Feat] : CouponIssueRequestDto의 triggerTypeCode 필드 타입 변경 및 유효성 검증 추가

--- src/main/java/shop/yesaladin/coupon/coupon/dto/CouponIssueRequestDto.java
+++ src/main/java/shop/yesaladin/coupon/coupon/dto/CouponIssueRequestDto.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import shop.yesaladin.coupon.trigger.TriggerTypeCode;
+import shop.yesaladin.coupon.validator.annotation.EnumValue;
 
 /**
  * 쿠폰 발급 요청시 사용하는 DTO 클래스입니다.
@@ -20,7 +21,8 @@ import shop.yesaladin.coupon.trigger.TriggerTypeCode;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CouponIssueRequestDto {
 
-    private TriggerTypeCode triggerTypeCode;
+    @EnumValue(enumClass = TriggerTypeCode.class)
+    private String triggerTypeCode;
 
     @PositiveOrZero(message = "invalid coupon id: ${validatedValue}")
     private Long couponId;
