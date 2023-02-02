7deefb1b89ef41554a7edeffb5f262904d2fe104
Hongdae Kim <mongmeo.dev@gmail.com>
2023-02-02 13:35:19 +0900

    [Feat] : QueryCouponGroupRepository 생성

---
+++ src/main/java/shop/yesaladin/coupon/coupon/persistence/QueryDslQueryCouponGroupRepository.java
@@ -0,0 +1,49 @@
+package shop.yesaladin.coupon.coupon.persistence;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import shop.yesaladin.coupon.coupon.domain.model.Coupon;
+import shop.yesaladin.coupon.coupon.domain.model.CouponGroup;
+import shop.yesaladin.coupon.coupon.domain.model.Trigger;
+import shop.yesaladin.coupon.coupon.domain.model.querydsl.QCouponGroup;
+import shop.yesaladin.coupon.coupon.domain.repository.QueryCouponGroupRepository;
+import shop.yesaladin.coupon.trigger.TriggerTypeCode;
+
+/**
+ * 쿠폰 그룹을 조회하기 위한 레포지토리 인터페이스의 구현체입니다.
+ *
+ * @author 김홍대
+ * @since 1.0
+ */
+@RequiredArgsConstructor
+@Repository
+public class QueryDslQueryCouponGroupRepository implements QueryCouponGroupRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Optional<CouponGroup> findCouponGroupByTrigger(Trigger trigger) {
+        QCouponGroup couponGroup = QCouponGroup.couponGroup;
+        return Optional.ofNullable(queryFactory.select(couponGroup)
+                .from(couponGroup)
+                .where(couponGroup.coupon.eq(trigger.getCoupon()))
+                .where(couponGroup.triggerTypeCode.eq(trigger.getTriggerTypeCode()))
+                .orderBy(couponGroup.createdDatetime.desc())
+                .fetchFirst());
+    }
+
+    @Override
+    public Optional<CouponGroup> findCouponGroupByCouponAndTriggerTypeCode(
+            Coupon coupon, TriggerTypeCode triggerTypeCode
+    ) {
+        QCouponGroup couponGroup = QCouponGroup.couponGroup;
+        return Optional.ofNullable(queryFactory.select(couponGroup)
+                .from(couponGroup)
+                .where(couponGroup.coupon.eq(coupon))
+                .where(couponGroup.triggerTypeCode.eq(triggerTypeCode))
+                .orderBy(couponGroup.createdDatetime.desc())
+                .fetchFirst());
+    }
+}
