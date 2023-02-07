01a090a27458b5c507bd5a281f851752ef823092
서민지 <squarearth1@gmail.com>
2023-02-07 16:29:32 +0900

    [Fix]: 사용불가능한 발행쿠폰 조회 쿼리 수정

--- src/main/java/shop/yesaladin/coupon/coupon/persistence/QueryDslQueryIssuedCouponRepository.java
+++ src/main/java/shop/yesaladin/coupon/coupon/persistence/QueryDslQueryIssuedCouponRepository.java
@@ -97,9 +97,9 @@ public class QueryDslQueryIssuedCouponRepository implements QueryIssuedCouponRep
         QIssuedCoupon issuedCoupon = QIssuedCoupon.issuedCoupon;
 
         return queryFactory.selectFrom(issuedCoupon).where(
-                issuedCoupon.couponGivenStateCode.ne(CouponGivenStateCode.GIVEN),
-                issuedCoupon.couponUsageStateCode.ne(CouponUsageStateCode.NOT_USED),
-                issuedCoupon.expirationDate.before(requestDateTime.toLocalDate())
+                issuedCoupon.couponGivenStateCode.ne(CouponGivenStateCode.GIVEN).or(
+                        issuedCoupon.couponUsageStateCode.ne(CouponUsageStateCode.NOT_USED)).or(
+                        issuedCoupon.expirationDate.before(requestDateTime.toLocalDate()))
         ).fetch();
     }
 }
