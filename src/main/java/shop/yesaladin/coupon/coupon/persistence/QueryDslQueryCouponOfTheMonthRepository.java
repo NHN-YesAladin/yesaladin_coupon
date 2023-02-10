458a55ce1a1b4567904a0bc8aefaaa0a9e10631e
서민지 <squarearth1@gmail.com>
2023-02-10 23:39:26 +0900

    [Feat]: 이달의 쿠폰 정책 테이블 조회 repository, service 작성

--- src/main/java/shop/yesaladin/coupon/coupon/persistence/QueryDslQueryCouponOfTheMonthRepository.java
+++ src/main/java/shop/yesaladin/coupon/coupon/persistence/QueryDslQueryCouponOfTheMonthRepository.java
@@ -1,2 +1,34 @@
-package shop.yesaladin.coupon.coupon.persistence;public class QueryDslQueryCouponOfTheMonthRepository {
+package shop.yesaladin.coupon.coupon.persistence;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import shop.yesaladin.coupon.coupon.domain.model.CouponOfTheMonthPolicy;
+import shop.yesaladin.coupon.coupon.domain.model.querydsl.QCouponOfTheMonthPolicy;
+import shop.yesaladin.coupon.coupon.domain.repository.QueryCouponOfTheMonthPolicyRepository;
+
+/**
+ * 이달의 쿠폰 정책을 조회하는 Repository 의 구현 클래스입니다.
+ */
+@RequiredArgsConstructor
+@Repository
+public class QueryDslQueryCouponOfTheMonthRepository implements
+        QueryCouponOfTheMonthPolicyRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Optional<CouponOfTheMonthPolicy> findFirstByOrderByIdDesc() {
+        QCouponOfTheMonthPolicy policy = QCouponOfTheMonthPolicy.couponOfTheMonthPolicy;
+
+        return Optional.ofNullable(queryFactory.selectFrom(policy)
+                .innerJoin(policy.coupon)
+                .fetchJoin()
+                .orderBy(policy.id.desc())
+                .fetchFirst());
+    }
 }
