ccc76e80650fdc0ffb7d67091e9664a9e2c24874
Hongdae Kim <mongmeo.dev@gmail.com>
2023-02-11 14:57:12 +0900

    [Feat] : 트리거 타입 코드로 쿠폰 조회 서비스 메서드 추가

--- src/test/java/shop/yesaladin/coupon/coupon/service/impl/QueryCouponServiceImplTest.java
+++ src/test/java/shop/yesaladin/coupon/coupon/service/impl/QueryCouponServiceImplTest.java
@@ -12,11 +12,13 @@ import org.mockito.Mockito;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import shop.yesaladin.coupon.code.CouponTypeCode;
 import shop.yesaladin.coupon.code.TriggerTypeCode;
 import shop.yesaladin.coupon.coupon.domain.model.Coupon;
 import shop.yesaladin.coupon.coupon.domain.model.PointCoupon;
 import shop.yesaladin.coupon.coupon.domain.model.Trigger;
+import shop.yesaladin.coupon.coupon.domain.repository.QueryCouponRepository;
 import shop.yesaladin.coupon.coupon.domain.repository.QueryIssuedCouponRepository;
 import shop.yesaladin.coupon.coupon.domain.repository.QueryTriggerRepository;
 import shop.yesaladin.coupon.coupon.dto.CouponSummaryDto;
@@ -26,12 +28,18 @@ class QueryCouponServiceImplTest {
     private QueryTriggerRepository queryTriggerRepository;
     private QueryCouponServiceImpl queryCouponService;
     private QueryIssuedCouponRepository queryIssuedCouponRepository;
+    private QueryCouponRepository queryCouponRepository;
 
     @BeforeEach
     void setUp() {
         queryTriggerRepository = Mockito.mock(QueryTriggerRepository.class);
         queryIssuedCouponRepository = Mockito.mock(QueryIssuedCouponRepository.class);
-        queryCouponService = new QueryCouponServiceImpl(queryTriggerRepository, queryIssuedCouponRepository);
+        queryCouponRepository = Mockito.mock(QueryCouponRepository.class);
+        queryCouponService = new QueryCouponServiceImpl(
+                queryTriggerRepository,
+                queryIssuedCouponRepository,
+                queryCouponRepository
+        );
     }
 
     @Test
@@ -68,8 +76,8 @@ class QueryCouponServiceImplTest {
         List<CouponSummaryDto> couponSummaryDtos = new ArrayList<>();
         couponSummaryDtos.add(couponSummaryDto);
         PageImpl<CouponSummaryDto> couponSummaryDtoPage = new PageImpl<>(couponSummaryDtos);
-        Mockito.when(queryTriggerRepository.findAll(Mockito.any())).thenReturn(couponSummaryDtoPage);
-
+        Mockito.when(queryTriggerRepository.findAll(Mockito.any()))
+                .thenReturn(couponSummaryDtoPage);
 
         // when
         Page<CouponSummaryDto> result = queryCouponService.getTriggeredCouponList(
@@ -79,6 +87,30 @@ class QueryCouponServiceImplTest {
         Assertions.assertThat(result.getContent()).hasSize(1);
         Assertions.assertThat(result.getContent().get(0).getName()).isEqualTo(name);
         Assertions.assertThat(result.getContent().get(0).getChargePointAmount()).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("트리거 타입 코드로 쿠폰 조회에 성공한다.")
+    @SuppressWarnings("unchecked")
+    void getCouponListByTriggerTypeCodeTest() {
+        // given
+        Page<CouponSummaryDto> expectedReturnValue = Mockito.mock(Page.class);
+        Pageable expectedPageable = PageRequest.of(0, 10);
+        Mockito.when(queryCouponRepository.findCouponByTriggerCode(
+                TriggerTypeCode.SIGN_UP,
+                expectedPageable
+        )).thenReturn(expectedReturnValue);
+
+        // when
+        Page<CouponSummaryDto> actual = queryCouponService.getCouponListByTriggerTypeCode(
+                TriggerTypeCode.SIGN_UP,
+                expectedPageable
+        );
+
+        // then
+        Assertions.assertThat(actual).isEqualTo(expectedReturnValue);
+        Mockito.verify(queryCouponRepository, Mockito.times(1))
+                .findCouponByTriggerCode(TriggerTypeCode.SIGN_UP, expectedPageable);
     }
 
 }
