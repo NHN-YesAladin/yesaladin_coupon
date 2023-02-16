0b83e2b1dc66c297480d06073c55560c6542a5a0
서민지 <squarearth1@gmail.com>
2023-02-16 17:40:46 +0900

    [Refactor]: CommandCouponService 리팩토링

--- src/main/java/shop/yesaladin/coupon/coupon/service/impl/CouponOfTheMonthServiceImpl.java
+++ src/main/java/shop/yesaladin/coupon/coupon/service/impl/CouponOfTheMonthServiceImpl.java
@@ -1,12 +1,15 @@
 package shop.yesaladin.coupon.coupon.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shop.yesaladin.common.code.ErrorCode;
 import shop.yesaladin.common.exception.ClientException;
 import shop.yesaladin.coupon.coupon.domain.model.CouponOfTheMonthPolicy;
+import shop.yesaladin.coupon.coupon.domain.repository.CommandCouponOfTheMonthPolicyRepository;
 import shop.yesaladin.coupon.coupon.domain.repository.QueryCouponOfTheMonthPolicyRepository;
+import shop.yesaladin.coupon.coupon.dto.MonthlyCouponPolicyDto;
 import shop.yesaladin.coupon.coupon.service.inter.CouponOfTheMonthService;
 
 /**
@@ -15,10 +18,12 @@ import shop.yesaladin.coupon.coupon.service.inter.CouponOfTheMonthService;
  * @author 서민지
  * @since 1.0
  */
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class CouponOfTheMonthServiceImpl implements CouponOfTheMonthService {
 
+    private final CommandCouponOfTheMonthPolicyRepository commandCouponOfTheMonthPolicyRepository;
     private final QueryCouponOfTheMonthPolicyRepository queryCouponOfTheMonthPolicyRepository;
 
     /**
@@ -28,7 +33,27 @@ public class CouponOfTheMonthServiceImpl implements CouponOfTheMonthService {
     @Transactional(readOnly = true)
     public CouponOfTheMonthPolicy getLatestPolicy() {
         return queryCouponOfTheMonthPolicyRepository.findLatestCouponOfTheMonthPolicy().orElseThrow(
-                () -> new ClientException(ErrorCode.COUPON_POLICY_NOT_FOUND, "이달의 쿠폰 정책이 존재하지 않습니다.")
+                () -> new ClientException(
+                        ErrorCode.COUPON_POLICY_NOT_FOUND,
+                        "이달의 쿠폰 정책이 존재하지 않습니다."
+                )
         );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @Transactional
+    public void createPolicy(MonthlyCouponPolicyDto dto) {
+        CouponOfTheMonthPolicy policy = CouponOfTheMonthPolicy.builder()
+                .coupon(dto.getCoupon())
+                .openTime(dto.getOpenTime())
+                .openDate(dto.getOpenDate())
+                .quantity(dto.getCouponQuantity())
+                .createdDateTime(null)
+                .build();
+
+        commandCouponOfTheMonthPolicyRepository.save(policy);
     }
 }
