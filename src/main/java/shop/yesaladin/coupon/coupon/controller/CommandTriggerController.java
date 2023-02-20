46431cec1160502151d85e3b16dc8f2a0c7ad188
서민지 <squarearth1@gmail.com>
2023-02-20 13:10:28 +0900

    [Feat]: 쿠폰 발행 중단 요청 @ModelAttribute > @RequestParam 변경

--- src/main/java/shop/yesaladin/coupon/coupon/controller/CommandTriggerController.java
+++ src/main/java/shop/yesaladin/coupon/coupon/controller/CommandTriggerController.java
@@ -3,12 +3,12 @@ package shop.yesaladin.coupon.coupon.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import shop.yesaladin.common.dto.ResponseDto;
-import shop.yesaladin.coupon.coupon.dto.CouponTriggerDto;
+import shop.yesaladin.coupon.code.TriggerTypeCode;
 import shop.yesaladin.coupon.coupon.service.inter.CommandTriggerService;
 
 @RequiredArgsConstructor
@@ -18,10 +18,13 @@ public class CommandTriggerController {
 
     private final CommandTriggerService commandTriggerService;
 
-    @DeleteMapping(params = {"trigger-code-id", "coupon-id"})
+    @DeleteMapping(params = {"trigger-type", "coupon-id"})
     @ResponseStatus(HttpStatus.OK)
-    public ResponseDto<Void> stopIssueCoupon(@ModelAttribute CouponTriggerDto dto) {
-        commandTriggerService.stopIssueCoupon(dto.getTriggerTypeCode(), dto.getCouponId());
+    public ResponseDto<Void> stopIssueCoupon(
+            @RequestParam(name = "trigger-type") TriggerTypeCode triggerTypeCode,
+            @RequestParam(name = "coupon-id") Long couponId
+    ) {
+        commandTriggerService.stopIssueCoupon(triggerTypeCode, couponId);
 
         return ResponseDto.<Void>builder()
                 .success(true)
--- src/main/java/shop/yesaladin/coupon/coupon/service/impl/CommandTriggerServiceImpl.java
+++ src/main/java/shop/yesaladin/coupon/coupon/service/impl/CommandTriggerServiceImpl.java
@@ -2,6 +2,7 @@ package shop.yesaladin.coupon.coupon.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import shop.yesaladin.coupon.code.TriggerTypeCode;
 import shop.yesaladin.coupon.coupon.persistence.JpaCommandTriggerRepository;
 import shop.yesaladin.coupon.coupon.service.inter.CommandTriggerService;
@@ -13,6 +14,7 @@ public class CommandTriggerServiceImpl implements CommandTriggerService {
     private final JpaCommandTriggerRepository commandTriggerRepository;
 
     @Override
+    @Transactional
     public void stopIssueCoupon(TriggerTypeCode triggerTypeCode, long couponId) {
         commandTriggerRepository.deleteByTriggerTypeCodeAndCoupon_Id(triggerTypeCode, couponId);
     }
